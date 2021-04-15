7c28d2766f0d0a761727cd2dfce5d6077b954f28
Ben Fan <Benjamin.Fan1@T-Mobile.com>
2021-04-14 22:28:05 -0600

    Rehaul with docker, graphql and auth

---
+++ run.sh
@@ -0,0 +1,2 @@
+docker build -t nest-playground .
+docker run -d -t -p 3000:3000 nest-playground
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,42 @@
-import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { AppController } from './app.controller';
-import { UserService } from './user.service';
-import { PostService } from './post.service';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
-import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
+import { AuthModule } from './resolvers/auth/auth.module';
+import { UserModule } from './resolvers/user/user.module';
+import { PostModule } from './resolvers/post/post.module';
+import { AppResolver } from './resolvers/app.resolver';
+import { DateScalar } from './common/scalars/date.scalar';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './configs/config';
+import { GraphqlConfig } from './configs/config.interface';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    GraphQLModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const graphqlConfig = configService.get<GraphqlConfig>('graphql');
+        return {
+          installSubscriptionHandlers: true,
+          buildSchemaOptions: {
+            numberScalarMode: 'integer',
+          },
+          sortSchema: graphqlConfig.sortSchema,
+          autoSchemaFile:
+            graphqlConfig.schemaDestination || './src/schema.graphql',
+          debug: graphqlConfig.debug,
+          playground: graphqlConfig.playgroundEnabled,
+          context: ({ req }) => ({ req }),
+        };
+      },
+      inject: [ConfigService],
     }),
+    AuthModule,
+    UserModule,
+    PostModule,
   ],
   controllers: [AppController],
-  providers: [AppService, PostService, UserService, PrismaService],
+  providers: [AppService, AppResolver, DateScalar],
 })
 export class AppModule {}
